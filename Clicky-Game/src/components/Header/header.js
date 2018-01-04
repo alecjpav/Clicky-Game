059aa20e5c7578bb91a6d62f133af4554bc0733c
unknown <alecjames.pav@gmail.com>
2018-01-04 15:19:49 -0700

    Initial commit

---
+++ Clicky-Game/src/components/Header/header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './header.css'
+
+const Header = () => {
+	return (
+		<header className="header">
+			<h1>Clicky Game - Don't click the same picture twice!</h1> 
+			
+		</header>
+	)
+}
+
+export default Header
